c8fa3a635ad14eb5564e9dc1ee13d5ca6b5d78bc
Kayla Hansen <shiverr@gmail.com>
2017-09-04 03:28:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var name = "Susan"
-var height = "74"
+var height = "70"
 
 if (name === "Susan") {
   console.log("The name is correct")
@@ -7,11 +7,6 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 
-if (height === 74) {
-  console.log("The name is corect")
-}  else {
-  console.log("Expected height to be 74")
-}
 
 
 // Don't worry about this
